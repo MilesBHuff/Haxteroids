64e8463f0dcc3196981ac95e01d528b443bd7e9b
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2022-05-13 18:45:40 -0400

    foo

--- src/incl/meta.js
+++
@@ -1,4 +0,0 @@
-// document.write("<link rel=icon type=image/x-icon href=incl/favicon.ico>");
-document.write("<link rel=stylesheet type=text/css href=incl/style.css>");
-document.write("<meta charset=UTF-8>");
-// document.write("<script src=http://cobweb.cs.uga.edu/~huff/incl/analytics.js></script>");
